49f9b80f32ad49cafb4519c2bfdef18eae54f432
Bruno Candia <bruno.candia@endava.com>
2025-05-16 16:02:23 -0300

    Angular basics done for section 8 adn tailwind v3 installed

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./layout/header/header.component";
+import { HttpClient } from '@angular/common/http';
+import { Product } from './shared/models/product';
+import { Pagination } from './shared/models/pagination';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'Client';
+export class AppComponent implements OnInit {
+  baseUrl = "https://localhost:7130/api/"
+  title = 'Skinet';
+  products: Product[] = [];
+  
+  constructor(private httpClient: HttpClient) {}
+  
+  ngOnInit(): void {
+    this.httpClient.get<Pagination<Product>>(this.baseUrl + 'products').subscribe({
+      next: response => this.products = response.data,
+      error: error => console.error(error),
+      complete: () => console.log('complete')
+    })
+  }
 }
